fa58de5d48432c1342079d3aff96763ba19c742d
drzagidullina <drzagidullina@ya.ru>
2019-11-05 12:06:35 +0300

    Added argparser for better launching

--- main.py
+++ main.py
@@ -2,35 +2,44 @@ from __future__ import print_function
 import argparse
 import os
 import numpy
+from torchvision.datasets import ImageFolder
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision.datasets import ImageFolder
 from PIL import Image
 import numpy as np
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
-def make_data_mnist(batch_size_train=64, batch_size_test = 1000):
+def make_data(args, kwargs):
+    if args.dataset == 'MNIST':
+        return make_data_mnist(args, kwargs)
+
+def make_model(args, device):
+    if args.model == 'example':
+        model = ExampleNet().to(device)
+    return model
+    
+def make_data_mnist(args, kwargs):
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
-        batch_size=batch_size_train, shuffle=True)
+        batch_size=args.batch_size, shuffle=True, **kwargs)
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=False, transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
-        batch_size=batch_size_test, shuffle=True)
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
     return train_loader, test_loader
 
-class Net(nn.Module):
+class ExampleNet(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
+        super(ExampleNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 20, 5, 1)
         self.conv2 = nn.Conv2d(20, 50, 5, 1)
         self.fc1 = nn.Linear(4*4*50, 500)
@@ -46,7 +55,7 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
-def train(model, device, train_loader, optimizer, epoch, log_interval=10):
+def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -55,12 +64,13 @@ def train(model, device, train_loader, optimizer, epoch, log_interval=10):
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % log_interval == 0:
+        if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
 
-def test(model, device, test_loader):
+
+def test(args, model, device, test_loader):
     model.eval()
     test_loss = 0
     correct = 0
@@ -79,27 +89,47 @@ def test(model, device, test_loader):
         100. * correct / len(test_loader.dataset)))
 
 def main():
-    #prepare args
-    n_epochs = 3
-    no_cuda = True
-    torch.backends.cudnn.enabled = False
-    random_seed = 1
-    torch.manual_seed(random_seed)
-    use_cuda = not no_cuda and torch.cuda.is_available()
-    device = torch.device("cuda" if use_cuda else "cpu")
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--model', choices=['example'], default='example',
+                        help='model name')
+    parser.add_argument('--dataset', choices=['MNIST'], default='MNIST',
+                        help='dataset')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
     
-    model = Net().to(device)
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
 
-    #prepare optimizer
-    learning_rate = 0.01
-    momentum = 0.5
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+    torch.manual_seed(args.seed)
 
-    train_loader, test_loader = make_data_mnist()
+    device = torch.device("cuda" if use_cuda else "cpu")
 
-    for epoch in range(1, n_epochs + 1):
-        train(model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader)
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+
+    train_loader, test_loader = make_data(args, kwargs)
+    model = make_model(args, device)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader)
 
 if __name__ == '__main__':
     main()

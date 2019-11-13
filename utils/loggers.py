b4b95c4d5f7cc40e9abd9f8a7f5205139c9c3d27
ActiveChooN <chchchoon.dk@gmail.com>
2019-11-13 17:17:46 +0300

    Added ignite integration

--- utils/loggers.py
+++ utils/loggers.py
@@ -4,44 +4,41 @@ import os
 
 
 class BaseLogger:
-    def __init__(self, log_interval):
+    def __init__(self, log_interval, train_len):
         self.log_interval = log_interval
-        self.step = 0
-        self._final_accuracy = 0.
-        self._final_loss = 0.
+        self.train_len = train_len
+        self.metrics = {}
         self.__cometml_api_key = os.environ.get("COMETML_API_KEY")
         if self.__cometml_api_key:
             self.experiment = Experiment(self.__cometml_api_key,
                                          project_name="sirius-adversarial-attack")
 
-    def log_test(self, logs_dict):
-        if not ("accuracy" in logs_dict.keys() and "loss" in logs_dict.keys()):
-            raise Exception("Loss and Accuracy are necessary parameters")
-        self._final_accuracy = logs_dict["accuracy"]
-        self._final_loss = logs_dict["loss"]
-        logging.info(f'----------------------------------------\n'
-                     f'TEST: Avg. loss: {logs_dict["loss"]:.5f}, '
-                     f'Accuracy: {logs_dict["accuracy"]:.5f}')
+    def log_test(self, engine):
+        self.metrics = engine.state.metrics
+        logging.info('----------------------------------------')
+        logging.info(f'TEST: Epoch:[{engine.state.epoch}]')
+        logging.info(f', '.join([
+            f'{name}: {self.metrics[name]:.5f}' for name in self.metrics
+        ]))
         logging.info(f'----------------------------------------')
         if self.__cometml_api_key:
-            self.experiment.log_metrics(logs_dict, prefix='Test')
+            self.experiment.log_metrics(self.metrics, prefix='Test')
 
-    def log_train(self, logs_dict):
-        if not ("loss" in logs_dict.keys() and "epoch" in logs_dict.keys() and
-                "progress" in logs_dict.keys()):
-            raise Exception("Loss, epoch and progress are necessary parameters")
-        if self.step % self.log_interval == 0:
-            progress = logs_dict.pop("progress")
-            epoch = logs_dict.pop("epoch")
-            logging.info(f'Epoch: {epoch}, progress: {progress:.0f}%, '
-                         f'loss {logs_dict["loss"]:.5f}')
+    def log_train(self, engine):
+        if engine.state.iteration % self.train_len % self.log_interval == 0:
+            logging.info("Epoch[{}] Iteration[{}/{}] Loss: {:.5f}"
+                         .format(engine.state.epoch, engine.state.iteration %
+                                 self.train_len if engine.state.iteration
+                                 % self.train_len else self.train_len,
+                                 self.train_len, engine.state.output))
             if self.__cometml_api_key:
-                self.experiment.log_metrics(logs_dict, step=self.step,
-                                            epoch=epoch, prefix='Train')
-        self.step += 1
+                logs_dict = {"loss": engine.state.output}
+                self.experiment.log_metrics(logs_dict,
+                                            step=engine.state.iteration,
+                                            epoch=engine.state.epoch,
+                                            prefix='Train')
 
     def log_hparams(self, hparams_dict):
-        hparams_dict.update({'Loss': self._final_loss,
-                             'Accuracy': self._final_accuracy})
+        hparams_dict.update(self.metrics)
         if self.__cometml_api_key:
             self.experiment.log_parameters(hparams_dict)
